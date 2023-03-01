6bc2f484ca6d64109dec20565641eb8ae52332ce
Abhishek Kumar <abhishekkumar6665@gmail.com>
2023-03-01 19:12:42 +0530

    Time: 2 ms (52.72%), Space: 7.7 MB (40.12%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(),stones.end()); 
+        while(pq.size()!=1)
+        {
+            int a = pq.top(); 
+            pq.pop();
+            int b = pq.top(); 
+            pq.pop();
+            pq.push(a-b);
+        }
+        return pq.top();
+
+    }
+};
